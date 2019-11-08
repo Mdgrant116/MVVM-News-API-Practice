50fdd137e924b8a060f62303c1ac8625cd0986ca
Michael Grant <Keepitfresh.us@gmail.com>
2019-11-08 13:57:23 -0500

    Initial Commit

---
+++ MVVM News API Practice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVVM News API Practice
+//
+//  Created by Michael Grant on 11/8/19.
+//  Copyright © 2019 Michael Grant. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
