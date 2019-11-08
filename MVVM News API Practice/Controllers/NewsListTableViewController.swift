2de50f713477c68fab4f76011460c2b2c5597f1e
Michael Grant <Keepitfresh.us@gmail.com>
2019-11-08 16:05:52 -0500

    Init

---
+++ MVVM News API Practice/Controllers/NewsListTableViewController.swift
@@ -0,0 +1,26 @@
+//
+//  NewsListTableViewController.swift
+//  MVVM News API Practice
+//
+//  Created by Michael Grant on 11/8/19.
+//  Copyright © 2019 Michael Grant. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+
+class NewsListTableViewController: UITableViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+//        setup()
+    }
+    
+    private func setup() {
+        self.navigationController?.navigationBar.prefersLargeTitles = true 
+        
+    }
+}
